6b4be3799cffa42c2f4df951e09d3b10258725a4
Luis Navarrete <goku_web5@hotmail.com>
2025-02-27 18:46:42 -0600

    primer commit

---
+++ server/server.js
@@ -0,0 +1,37 @@
+// needed imports
+const express = require('express');
+const path = require('path');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+require('dotenv').config();
+
+// Middleware
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../front')));
+
+// Routes for views
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../front/views/index.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '../front/views/login.html'));
+});
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '../front/views/register.html'));
+});
+
+// basic Socket.io settings
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+// Port Settings and server start
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
